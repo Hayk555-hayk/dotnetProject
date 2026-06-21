0129126738c4c03df45c382e30ce957a50f30ffc
Hayk <ayk.khachatryan.1994@gmail.com>
2026-06-21 13:47:10 +0400

    DB connection

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,8 +1,10 @@
-class Product
+namespace dotnetProject.Models;
+
+public class Product
 {
-    public int ID {get; set;}
-    public string Name {get; set;} = string.Empty;
-    public decimal Price {get; set;}
-    public string? Description {get; set;}
+    public int ID { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,25 +1,32 @@
+using dotnetProject.Models;
+using dotnetProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 var app = builder.Build();
-
-
-var products = new List<Product>
-{
-    new Product { ID = 1, Name = "Product 1", Price = 10.99m, Description = "Description for Product 1" },
-    new Product { ID = 2, Name = "Product 2", Price = 19.99m, Description = "Description for Product 2" },
-    new Product { ID = 3, Name = "Product 3", Price = 29.99m, Description = "Description for Product 3" }
-};
-
 
 app.MapGet("/", () => "Api is running !");
 app.MapGet("/api/welcome", () => "Welcome to my API !");
 
-app.MapGet("/api/products", () => products);
+app.MapGet("/api/products", async (AppDbContext context) => await context.Products.ToListAsync());
 
-app.MapPost("/api/products", (Product newProduct) => {
-    newProduct.ID = products.Count + 1; 
-    products.Add(newProduct);
-    return Results.Created($"/api/products/{newProduct.ID}", newProduct);
+app.MapGet("/api/products/{id}", async (AppDbContext context, int id) =>
+{
+    var product = context.Products.Find(id);
+    return product is not null ? Results.Ok(product) : Results.NotFound();
+});
+
+app.MapPost("/api/products", async (AppDbContext context, Product newProduct) => {
+    context.Products.Add(newProduct);
+        await context.SaveChangesAsync();
+        return Results.Created($"/api/products/{newProduct.ID}", newProduct);
 });
 
 app.Run();
