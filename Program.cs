4ebc619bfaef46f11de62eaae3a55eac9cb2849b
Hayk <ayk.khachatryan.1994@gmail.com>
2026-06-22 11:53:09 +0400

    Cors

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 using dotnetProject.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -14,5 +25,6 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapControllers();
+app.UseCors("AllowReactApp");
 
 app.Run();
