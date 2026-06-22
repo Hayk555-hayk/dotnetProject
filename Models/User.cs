7db32525e663d000dbdbb00acbf3d50953cf98bd
Hayk <ayk.khachatryan.1994@gmail.com>
2026-06-22 15:32:32 +0400

    User model

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnetProject.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Email обязателен для заполнения")]
+    [EmailAddress(ErrorMessage = "Некорректный формат Email адреса")]
+    [StringLength(100, ErrorMessage = "Email не должен превышать 100 символов")]
+    public required string Email { get; set; }
+    [Required(ErrorMessage = "Пароль обязателен для заполнения")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+}
